e86e97db9d3ccf5cf659fbb668e5ed92e3723445
JoakimHillberg <joakim.hillberg@elev.ga.ntig.se>
2024-05-23 21:46:50 +0200

    Nätverkslabb, A-nivå, gör om min kod då split inte fungerade särskilt bra.

--- Joakim_Klient/src/Main.java
+++ Joakim_Klient/src/Main.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.net.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Socket client = null;
 
         // Port number we are going to use
@@ -10,38 +10,42 @@ public class Main {
 
         String msg = "";
 
-        // Create a client socket
-        client = new Socket(InetAddress.getLocalHost(), portNumber);
-        System.out.println("Client socket is created " + client);
+        try {
+            // Create a client socket
+            client = new Socket(InetAddress.getLocalHost(), portNumber);
+            System.out.println("Client socket is created " + client);
 
-        // Create an output stream of the client socket
-        OutputStream clientOut = client.getOutputStream();
-        PrintWriter pw = new PrintWriter(clientOut,true);
+            // Create an output stream of the client socket
+            OutputStream clientOut = client.getOutputStream();
+            PrintWriter pw = new PrintWriter(clientOut,true);
 
-        // Create an input stream of the client socket
-        InputStream clientIn = client.getInputStream();
-        BufferedReader br = new BufferedReader(new InputStreamReader(clientIn));
+            // Create an input stream of the client socket
+            InputStream clientIn = client.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(clientIn));
 
-        // Create BufferedReader for a standard input
-        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+            // Create BufferedReader for a standard input
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
 
-        while (true) {
-            System.out.println("Enter a number to calculate(use +,-,* or /)");
+            while (true) {
+                System.out.println("Enter a number to calculate(use +,-,* or /)");
 
-            // Read data from standard input device and write it
-            // to the output stream from the client socket.
-            msg = stdIn.readLine().trim();
-            pw.println(msg);
+                // Read data from standard input device and write it
+                // to the output stream from the client socket.
+                msg = stdIn.readLine().trim();
+                pw.println(msg);
 
-            // Read data from the input stream of the client socket.
-            System.out.println(br.readLine());
+                // Read data from the input stream of the client socket.
+                System.out.println(br.readLine());
 
-            if (msg.equalsIgnoreCase("bye")) {
-                pw.close();
-                br.close();
-                client.close();
-                break;
+                if (msg.equalsIgnoreCase("bye")) {
+                    pw.close();
+                    br.close();
+                    client.close();
+                    break;
+                }
             }
+        } catch (IOException ie) {
+            System.out.println("I/O error " + ie);
         }
     }
 }
--- Joakim_Server/src/Main.java
+++ Joakim_Server/src/Main.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.net.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         ServerSocket server = null;
         Socket client;
 
@@ -12,95 +12,45 @@ public class Main {
         int portNumber = 1234;
 
         // Create server side socket
-        try {
-            server = new ServerSocket(portNumber);
-        } catch (IOException ie) {
-            System.out.println("Cannot open socket. " + ie);
-            System.exit(1);
-        }
+        server = new ServerSocket(portNumber);
         System.out.println("ServerSocket is created " + server);
 
-        // Wait for the data from the client and reply
+        // Listens for a connection to be made to
+        // this socket and accepts it. The method blocks
+        // a connection is made
+        System.out.println("Waiting for connect request...");
+        client = server.accept();
+
+        System.out.println("Connect request is accepted...");
+        String clientHost = client.getInetAddress().getHostAddress();
+        int clientPort = client.getPort();
+        System.out.println("Client host = " + clientHost + " Client port = " + clientPort);
+
         while (true) {
-            try {
-                // Listens for a connection to be made to
-                // this socket and accepts it. The method blocks
-                // a connection is made
-                System.out.println("Waiting for connect request...");
-                client = server.accept();
+            // Read data from the client
+            InputStream clientIn = client.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(clientIn));
+            String msgFromClient = br.readLine();
+            System.out.println(msgFromClient);
 
-                System.out.println("Connect request is accepted...");
-                String clientHost = client.getInetAddress().getHostAddress();
-                int clientPort = client.getPort();
-                System.out.println("Client host = " + clientHost + " Client port = " + clientPort);
+            // Calculate value
 
-                // Read data from the client
-                InputStream clientIn = client.getInputStream();
-                BufferedReader br = new BufferedReader(new InputStreamReader(clientIn));
-                String msgFromClient = br.readLine();
-                System.out.println(msgFromClient);
 
-                // Calculate value
-                String symbol = getSymbol(msgFromClient);
-                String[] numbers = msgFromClient.split(symbol);
-                String response = "";
-                if (!symbol.isEmpty() && canParse(numbers[0]) && canParse(numbers[1])) {
-                    response = Integer.toString(calculate(symbol,Integer.parseInt(numbers[0]),Integer.parseInt(numbers[1])));
-                }
-
-                // Send response to client
-                OutputStream clientOut = client.getOutputStream();
-                PrintWriter pw = new PrintWriter(clientOut,true);
-                String ans = "Message from server: ";
-                if (msgFromClient.equalsIgnoreCase("bye")) {
-                    pw.println(ans + "Goodbye");
-                    server.close();
-                    client.close();
-                    pw.close();
-                    br.close();
-                    break;
-
-                } else if (!symbol.isEmpty()) {
-                    pw.println(ans + "The answer becomes " + response);
-                } else {
-                    pw.println(ans + "That can not be calculated");
-                }
-
-            } catch (IOException ie) {
-                System.out.println("I/O error " + ie);
+            // Send response to client
+            OutputStream clientOut = client.getOutputStream();
+            PrintWriter pw = new PrintWriter(clientOut,true);
+            String ans = "Message from server: ";
+            if (msgFromClient.equalsIgnoreCase("bye")) {
+                pw.println(ans + "Goodbye");
+                server.close();
+                client.close();
+                pw.close();
+                br.close();
+                break;
+            } else {
+                pw.println(ans + "That can not be calculated");
             }
         }
     }
 
-    public static boolean canParse(String number) {
-        try {
-            Integer.parseInt(number);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
-    }
-
-    public static String getSymbol(String msg) {
-        if (msg.contains("/")) {
-            return "\\/";
-        } else if (msg.contains("*")) {
-            return "\\-";
-        } else if (msg.contains("+")) {
-            return "\\+";
-        } else if (msg.contains("-")) {
-            return "\\-";
-        }
-        return "";
-    }
-
-    public static int calculate(String symbol, int number, int number2) {
-        return switch (symbol) {
-            case "\\+" -> number + number2;
-            case "\\-" -> number - number2;
-            case "\\*" -> number * number2;
-            case "\\/" -> number / number2;
-            default -> 0;
-        };
-    }
 }
